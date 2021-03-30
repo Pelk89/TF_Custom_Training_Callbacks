6bfd4d52b0e80fc7afed9565842a459f96dc8ff6
Pelkmann89 <scan.corona@gmail.com>
2021-03-30 23:36:55 +0200

    added comments

--- README.md
+++ README.md
@@ -26,6 +26,48 @@ model.fit(dataset, epochs=10, callbacks=my_callbacks)
 ```
 At each stage of the training (e.g. at the start or end of an epoch)  all relevant methods will be called automatically.
 
+
+---
+
+Tensorflow Custom Training with Custom Callbacks
+
+You want to use your low-level training and evaluation loops and don't want to miss the convenience of the Keras callbacks? No problem! In this tutorial, I will show you how to simply implement Keras callbacks in low-level training and evaluation loop. For this article, I assume that you are familiar with the basic terminology and principles of Machine Learning and have done some toy examples with Keras.
+HINT: When I first got in touch with Keras, I was overwhelmed with the different terminology of Machine Learning and was happy that I found the Machine Learning Glossary by Google.
+With a callback, it is possible to perform different actions at several stages while training your deep neural network. By default, Keras provides easy and convenient built-in callbacks like ReduceLROnPlateau (Reduce learning Rate on Plateau) or EarlyStopping for your training and evaluation loop. Their usage is covered in the .fit() method of a model and can be passed as a list of callbacks:
+
+At each stage of the training (e.g. at the start or end of an epoch) all relevant methods will be called automatically. You can find more detailed information about the callback methods in the Keras documentation. To write your own Callbacks I recommend the article Building Custom Callbacks with Keras and TensorFlow 2 by B. Chen. 
+I won't go into detail of how to implement a custom training loop. So if you want to have more information about that I recommend the Keras Classification Tutorial. For this tutorial we will slightly modify the mentioned classification tutorial for the MNIST dataset. The MNIST dataset contains handwritten digits and is commonly used as toy example for Machine Learning. Furthermore we will implement the widely used callback ReduceLROnPlateau and add also an exponential reducing of the learning rate. 
+This tutorial can be found on my Github. Use git clone to run it locally on your computer
+git clone https://github.com/Pelk89/TF_Custom_Training_Callbacks.git
+
+---
+
+Reduce Learning Rate On Plateau
+Reducing the learning rate is often used when a metric has stopped improving. Moreover once the learning stagnates Machine Learning Models often benefit from reducing the learning rate linearly. But what if we want to reduce the learning rate exponentially when the metric has stopped improving? No problem at all! To understand whats going on under the hood we need to go deeper into Keras library. Let's dig a little bit to the heart of the Keras Callbacks in tf.keras.callbacks.Callback. Thanks to Francois Chollet and the other authors we will find a incredible clean and comprehensible code for the class ReduceLROnPlateau. With this basis we easily can reuse the existing code and use it for our purposes. How great is that?
+
+When we analyze the class we will see for example the explanation for the arguments that can be passed to the __init__ method of the class. We will go into detail for the important arguements for this tutorial. A more detailed overview can be found at Keras ReduceLROnPlateau documentation.
+Arguments
+monitor: quantity (e.g. validation loss) to be monitored.
+factor: factor by which the learning rate will be reduced. new_lr = lr * factor.
+patience: number of epochs with no improvement after which learning rate will be reduced.
+cooldown: number of epochs to wait before resuming normal operation after learning rate has been reduced.
+
+And more importantly we can identify the underlying algorithm and when its called while training your neural network. The class uses two methods
+on_train_begin(): Called once when training begins and resetting wait and cooldown timer
+on_epoch_end(): Called on every end of an epoch and change the learning rate linearly depended on the defined cooldown timer and patience.
+
+Modify Reduce Learning Rate On Plateau
+Now that we have a detailed overview of the Reduce Learning Rate On Plateau algorithm, we are able to modify it for our needs. Moreover we can implement the algorithm at the right position in our custom training and evaluation loop. In this tutorial we will monitor the validation loss of our training model. First of all we need to know that we cant use the internal arguments self.model as the model is an instance of keras.models.Model and a reference of the model being trained. So everything with self.model needs to be replaced and passed as argument into the __init__ or on_epoch_end() method. The changes are marked as 
+## Custom modification: "Reason for Modification"
+If we want to reduce the learning rate exponentially we need to add a argument to __init__ method and also modify the method on_epoch_end(). 
+Modfiy __init__
+Because we measure the validation loss of our neural network we can remove the monitor argument. Also we want to set a boolean to set control whether we want to reduce the learning rate exponentially with reduce_exp and set its default value to false. Likewise we need to pass the optimizer to the method.
+
+Modify on_epoch_end()
+Now to the more complex part of the modification of the algorightm. Normally its method is called on every end of an epoch during training. In our case we additionally need to pass the loss of our validation dataset on every epoch end of our training to the method. 
+
+Putting it all together
+
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
---
+++ custom_callback.py
@@ -0,0 +1,166 @@
+import tensorflow as tf
+import numpy as np
+
+class CustomReduceLRoP():
+
+    """ Reduce learning rate when a metric has stopped improving.
+        Models often benefit from reducing the learning rate by a factor
+        of 2-10 once learning stagnates. This callback monitors a
+        quantity and if no improvement is seen for a 'patience' number
+        of epochs, the learning rate is reduced.
+
+        Example:
+        ```python
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,
+                                        patience=5, min_lr=0.001)
+        model.fit(X_train, Y_train, callbacks=[reduce_lr])
+        ```
+    Arguments:
+        monitor: quantity to be monitored.
+        factor: factor by which the learning rate will be reduced. new_lr = lr *
+            factor
+        patience: number of epochs with no improvement after which learning rate
+            will be reduced.
+        verbose: int. 0: quiet, 1: update messages.
+        mode: one of {auto, min, max}. In `min` mode, lr will be reduced when the
+            quantity monitored has stopped decreasing; in `max` mode it will be
+            reduced when the quantity monitored has stopped increasing; in `auto`
+            mode, the direction is automatically inferred from the name of the
+            monitored quantity.
+        min_delta: threshold for measuring the new optimum, to only focus on
+            significant changes.
+        cooldown: number of epochs to wait before resuming normal operation after
+            lr has been reduced.
+        min_lr: lower bound on the learning rate.
+        reduce_exp: reducing the learning rate exponentially
+    """
+
+
+
+
+
+    def __init__(self,
+                 ## Custom modification:  Deprecated
+                 # monitor='val_loss',
+                 factor=0.1,
+                 patience=10,
+                 verbose=0,
+                 mode='auto',
+                 min_delta=1e-4,
+                 cooldown=0,
+                 min_lr=0,
+                 sign_number = 4,
+                 ## Custom modification: Passing optimizer as arguement
+                 optim_lr = None,
+                 ## Custom modification:  Exponentially reducing learning
+                 reduce_exp = False,
+                 **kwargs):
+
+        ## Custom modification:  Deprecated
+        # super(ReduceLROnPlateau, self).__init__()
+
+        ## Custom modification:  Deprecated
+        # self.monitor = monitor
+        
+        ## Custom modification: Optimizer Error Handling
+        if tf.is_tensor(optim_lr) == False:
+            raise ValueError('Need optimizer !')
+        if factor >= 1.0:
+            raise ValueError('ReduceLROnPlateau ' 'does not support a factor >= 1.0.')
+        ## Custom modification: Passing optimizer as arguement
+        self.optim_lr = optim_lr  
+
+        self.factor = factor
+        self.min_lr = min_lr
+        self.min_delta = min_delta
+        self.patience = patience
+        self.verbose = verbose
+        self.cooldown = cooldown
+        self.cooldown_counter = 0  # Cooldown counter.
+        self.wait = 0
+        self.best = 0
+        self.mode = mode
+        self.monitor_op = None
+        self.sign_number = sign_number
+
+        ## Custom modification: Exponentially reducing learning
+        self.reduce_exp = reduce_exp
+        
+
+        self._reset()
+
+    def _reset(self):
+        """Resets wait counter and cooldown counter.
+        """
+        if self.mode not in ['auto', 'min', 'max']:
+            print('Learning Rate Plateau Reducing mode %s is unknown, '
+                            'fallback to auto mode.', self.mode)
+            self.mode = 'auto'
+        if (self.mode == 'min' or
+                (self.mode == 'auto' and 'acc' not in self.monitor)):
+            self.monitor_op = lambda a, b: np.less(a, b - self.min_delta)
+            self.best = np.Inf
+        else:
+            self.monitor_op = lambda a, b: np.greater(a, b + self.min_delta)
+            self.best = -np.Inf
+        self.cooldown_counter = 0
+        self.wait = 0
+
+    def train_begin(self, logs=None):
+        self._reset()
+
+    def epoch_end(self, epoch, loss, logs=None):
+
+
+        logs = logs or {}
+        ## Custom modification: Optimizer
+        # logs['lr'] = K.get_value(self.model.optimizer.lr) returns a numpy array
+        # and therefore can be modified to          
+        logs['lr'] = float(self.optim_lr.numpy())
+
+        ## Custom modification: Deprecated due to focusing on validation loss
+        # current = logs.get(self.monitor)
+
+        current = float(loss)
+        
+        ## Custom modification: Deprecated due to focusing on validation loss
+        # if current is None:
+        #     print('Reduce LR on plateau conditioned on metric `%s` '
+        #                     'which is not available. Available metrics are: %s',
+        #                     self.monitor, ','.join(list(logs.keys())))
+
+        # else:
+
+        if self.in_cooldown():
+            self.cooldown_counter -= 1
+            self.wait = 0
+
+        if self.monitor_op(current, self.best):
+            self.best = current
+            self.wait = 0
+        elif not self.in_cooldown():
+            self.wait += 1
+            if self.wait >= self.patience:
+                
+                ## Custom modification: Optimizer Learning Rate
+                # old_lr = float(K.get_value(self.model.optimizer.lr))
+                old_lr = float(self.optim_lr.numpy())
+                if old_lr > self.min_lr:
+                    ## Custom modification: Exponential learning Rate
+                    if self.reduce_exp == True:
+                        new_lr = old_lr * tf.math.exp(-0.1)
+                    else:
+                        new_lr = old_lr * self.factor
+                    new_lr = max(new_lr, self.min_lr)
+
+                    ## Custom modification: Optimizer Learning Rate
+                    # K.set_value(self.model.optimizer.lr, new_lr)
+                    self.optim_lr.assign(new_lr)
+                    if self.verbose > 0:
+                        print('\nEpoch %05d: ReduceLROnPlateau reducing learning '
+                                'rate to %s.' % (epoch + 1, float(new_lr)))
+                    self.cooldown_counter = self.cooldown
+                    self.wait = 0
+
+    def in_cooldown(self):
+        return self.cooldown_counter > 0
